e06ae73063fb76e2aa0b01dd39a78ed44a169141
amctech106 <121874402+amctech106@users.noreply.github.com>
2025-07-30 11:16:21 +0500

    Comparison Operators in JavaScript

---
+++ comparison operators/script.js
@@ -0,0 +1,12 @@
+let A = 15; 
+let B = 20;
+
+console.log( A == B); // False 
+console.log( A === B); // False 
+console.log( A != B); // true 
+console.log( A !== B); // true 
+console.log( A > B); // False 
+console.log( A < B); // true 
+console.log( A >= B); // False 
+console.log( A <= B); // true 
+
